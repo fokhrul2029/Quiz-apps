9f211ccdb027066b8c3457f753e5c7f36c46e182
fokhrul2029 <ifokhrul.2029@gmail.com>
2023-08-03 23:07:57 +0600

    first commit

--- asset/js/Questions.js
+++ asset/js/Questions.js
@@ -1,172 +1,92 @@
 let Questions = [
-      {
-        numb: 1 ,
-        question: `What is the past tense of the verb "to see"?`,
-        answer: "Saw",
-        options: [
-          "Saw",
-          "Seen",
-          "Seeed",
-          "Seeen"
-        ]
-      },
-      {
-        numb: 2 ,
-        question: `Identify the adjective in the following sentence: "The tall building is made of glass."`,
-        answer: "tall",
-        options: [
-          "building",
-          "tall",
-          "glass",
-          "made"
-        ]
-      },
-      {
-        numb: 3 ,
-        question: `Identify the pronoun in the following sentence: "He gave the book to them."`,
-        answer: "He",
-        options: [
-          "He",
-          "book",
-          "them",
-          "gave"
-        ]
-      },
-      {
-        numb: 4 ,
-        question: `What is the past participle of the verb "to speak"?`,
-        answer: "spoken",
-        options: [
-          "spoke",
-          "spoken",
-          "spoked",
-          "speaked"
-        ]
-      },
-      {
-        numb: 5 ,
-        question: `In the sentence "I have been studying for the test", what is the main verb?`,
-        answer: "studying",
-        options: [
-          "have",
-          "been",
-          "studying",
-          "test"
-        ]
-      },
-      {
-        numb: 6 ,
-        question: `The verb "to play" is ________ in the past tense.`,
-        answer: "Played",
-        options: [
-          "Play",
-          "Playe",
-          "Played",
-          "Plaied"
-        ]
-      },
-      {
-        numb: 7 ,
-        question: `The adjective in the following sentence is "happy": "I'm feeling ________ today."`,
-        answer: "happy",
-        options: [
-          "upset",
-          "happy",
-          "angry",
-          "sad"
-        ]
-      },
-      {
-        numb: 8 ,
-        question: `The preposition in the following sentence is "in": "He is hiding ________ the closet."`,
-        answer: "in",
-        options: [
-          "under",
-          "in",
-          "on",
-          "behind"
-        ]
-      },
-      {
-        numb: 9 ,
-        question: `The present participle of the verb "to eat" is _________.`,
-        answer: "eating",
-        options: [
-          "eat",
-          "ate",
-          "eating",
-          "eatting"
-        ]
-      },
-      {
-        numb: 10 ,
-        question: `The adverb in the following sentence is "quickly": "He ran ________ to catch the bus."`,
-        answer: "quickly",
-        options: [
-          "slow",
-          "quickly",
-          "fast",
-          "hardly"
-        ]
-      },
-      {
-        numb: 11 ,
-        question: `They are friends, ________?`,
-        answer: "aren't they",
-        options: [
-          "aren't they",
-          "isn't he",
-          "aren't we",
-          "isn't it"
-        ]
-      },
-      {
-        numb: 12 ,
-        question: `You have finished the project, _______?`,
-        answer: "haven't you",
-        options: [
-          "haven't they",
-          "haven't she",
-          "haven't it",
-          "haven't you"
-        ]
-      },
-      {
-        numb: 13 ,
-        question: `What is the plural form of "child"?`,
-        answer: "children",
-        options: [
-          "childs",
-          "childes",
-          "children",
-          "childeren"
-        ]
-      },
-      {
-        numb: 14 ,
-        question: `What is the past tense of the verb "to be"?`,
-        answer: "were",
-        options: [
-          "am",
-          "is",
-          "are",
-          "were"
-        ]
-      },
-      {
-        numb: 15 ,
-        question: `In the sentence "The mouse was chased by the cat," the subject is receiving the action. The sentence is in the ________ voice.`,
-        answer: "passive",
-        options: [
-          "active",
-          "passive",
-          "adjective",
-          "none"
-        ]
-      },
- 
-
-]
-
-
-
+  {
+    numb: 1,
+    question: `What is the past tense of the verb "to see"?`,
+    answer: "Saw",
+    options: ["Saw", "Seen", "Seeed", "Seeen"],
+  },
+  {
+    numb: 2,
+    question: `Identify the adjective in the following sentence: "The tall building is made of glass."`,
+    answer: "tall",
+    options: ["building", "tall", "glass", "made"],
+  },
+  {
+    numb: 3,
+    question: `Identify the pronoun in the following sentence: "He gave the book to them."`,
+    answer: "He",
+    options: ["He", "book", "them", "gave"],
+  },
+  {
+    numb: 4,
+    question: `What is the past participle of the verb "to speak"?`,
+    answer: "spoken",
+    options: ["spoke", "spoken", "spoked", "speaked"],
+  },
+  {
+    numb: 5,
+    question: `In the sentence "I have been studying for the test", what is the main verb?`,
+    answer: "studying",
+    options: ["have", "been", "studying", "test"],
+  },
+  {
+    numb: 6,
+    question: `The verb "to play" is ________ in the past tense.`,
+    answer: "Played",
+    options: ["Play", "Playe", "Played", "Plaied"],
+  },
+  {
+    numb: 7,
+    question: `The adjective in the following sentence is "happy": "I'm feeling ________ today."`,
+    answer: "happy",
+    options: ["upset", "happy", "angry", "sad"],
+  },
+  {
+    numb: 8,
+    question: `The preposition in the following sentence is "in": "He is hiding ________ the closet."`,
+    answer: "in",
+    options: ["under", "in", "on", "behind"],
+  },
+  {
+    numb: 9,
+    question: `The present participle of the verb "to eat" is _________.`,
+    answer: "eating",
+    options: ["eat", "ate", "eating", "eatting"],
+  },
+  {
+    numb: 10,
+    question: `The adverb in the following sentence is "quickly": "He ran ________ to catch the bus."`,
+    answer: "quickly",
+    options: ["slow", "quickly", "fast", "hardly"],
+  },
+  {
+    numb: 11,
+    question: `They are friends, ________?`,
+    answer: "aren't they",
+    options: ["aren't they", "isn't he", "aren't we", "isn't it"],
+  },
+  {
+    numb: 12,
+    question: `You have finished the project, _______?`,
+    answer: "haven't you",
+    options: ["haven't they", "haven't she", "haven't it", "haven't you"],
+  },
+  {
+    numb: 13,
+    question: `What is the plural form of "child"?`,
+    answer: "children",
+    options: ["childs", "childes", "children", "childeren"],
+  },
+  {
+    numb: 14,
+    question: `What is the past tense of the verb "to be"?`,
+    answer: "were",
+    options: ["am", "is", "are", "were"],
+  },
+  {
+    numb: 15,
+    question: `In the sentence "The mouse was chased by the cat," the subject is receiving the action. The sentence is in the ________ voice.`,
+    answer: "passive",
+    options: ["active", "passive", "adjective", "none"],
+  },
+];
--- asset/js/apps.js
+++ asset/js/apps.js
@@ -5,201 +5,183 @@
     // Description       : It's a simple Quiz Application and it's made by using pure js and js DOM. i have make it as a protfolio. There has a few simple question that can improve human's basic knowlage .
 */
 
-// Class and Element Selector 
-let firstPage = document.querySelector(".firstPage")
-let rulesPage = document.querySelector(".rulesPage")
-let quizPage = document.querySelector(".quizPage")
-let resultPage = document.querySelector(".resultPage")
-let timeStatus = document.querySelector(".timeStatus")
-let examT = document.querySelector(".examT")
-let DeveloperInfo = document.querySelector(".DeveloperInfo")
-let totalQueS = document.querySelector(".totalQueS"); 
-let timerBb = document.querySelector(".examTime .blockB")
+// Class and Element Selector
+let firstPage = document.querySelector(".firstPage");
+let rulesPage = document.querySelector(".rulesPage");
+let quizPage = document.querySelector(".quizPage");
+let resultPage = document.querySelector(".resultPage");
+let timeStatus = document.querySelector(".timeStatus");
+let examT = document.querySelector(".examT");
+let DeveloperInfo = document.querySelector(".DeveloperInfo");
+let totalQueS = document.querySelector(".totalQueS");
+let timerBb = document.querySelector(".examTime .blockB");
 
-let startQ = document.querySelector(".startQ")
-let exitQ = document.querySelector(".exitQ")
-let ContinueQ = document.querySelector(".ContinueQ")
-let nextQ = document.querySelector(".nextQ")
-let replayQ = document.querySelector(".replayQ")
-let quitQ = document.querySelector(".quitQ")
-let Timer = document.querySelector(".Timer")
-let result = document.querySelector(".result h4")
-let fkrl = document.querySelector(".fkrl")
-let backP = document.querySelector(".backP")
-let exitP = document.querySelector(".exitP")
+let startQ = document.querySelector(".startQ");
+let exitQ = document.querySelector(".exitQ");
+let ContinueQ = document.querySelector(".ContinueQ");
+let nextQ = document.querySelector(".nextQ");
+let replayQ = document.querySelector(".replayQ");
+let quitQ = document.querySelector(".quitQ");
+let Timer = document.querySelector(".Timer");
+let result = document.querySelector(".result h4");
+let fkrl = document.querySelector(".fkrl");
+let backP = document.querySelector(".backP");
+let exitP = document.querySelector(".exitP");
 
 //  EventListeners
-fkrl.onclick = () =>{
-    firstPage.classList.remove("active");
-    DeveloperInfo.classList.add("showInfo"); 
-}
+fkrl.onclick = () => {
+  firstPage.classList.remove("active");
+  DeveloperInfo.classList.add("showInfo");
+};
 backP.onclick = () => {
-    firstPage.classList.add("active")
-    DeveloperInfo.classList.remove("showInfo");
-}
+  firstPage.classList.add("active");
+  DeveloperInfo.classList.remove("showInfo");
+};
 exitP.onclick = () => {
-    firstPage.classList.add("active");
-    DeveloperInfo.setAttribute("class", "DeveloperInfo")
-}
+  firstPage.classList.add("active");
+  DeveloperInfo.setAttribute("class", "DeveloperInfo");
+};
 
+startQ.onclick = () => {
+  firstPage.classList.remove("active");
+  rulesPage.classList.add("active");
+};
+exitQ.onclick = () => {
+  firstPage.classList.add("active");
+  rulesPage.classList.remove("active");
+};
 
+replayQ.onclick = () => {
+  resultPage.classList.remove("active");
+  quizPage.classList.add("active");
+  replayQuiz();
+};
+quitQ.onclick = () => {
+  window.location.reload();
+};
 
+ContinueQ.onclick = () => {
+  quizPage.classList.add("active");
+  rulesPage.classList.remove("active");
+  showQuestions(que_count);
+  startTimer(timeValue);
+};
 
-startQ.onclick = ()=>{
-    firstPage.classList.remove("active");
-    rulesPage.classList.add("active");
-}
-exitQ.onclick = ()=>{
-    firstPage.classList.add("active");
-    rulesPage.classList.remove("active");
-}
-
-
-replayQ.onclick = ()=>{
-    resultPage.classList.remove("active");
-    quizPage.classList.add("active");
-    replayQuiz();
-}
-quitQ.onclick = ()=>{
-    window.location.reload();
-}
-
-ContinueQ.onclick = ()=>{
-    quizPage.classList.add("active");
-    rulesPage.classList.remove("active");
-    showQuestions(que_count)
-    startTimer(timeValue)
-}
-
-let que_count = 0 ;
-let counter ; 
-let timeValue = 25 ;        // EXAM'S TIME  
-let score=0 ; 
-Timer.textContent = timeValue ; 
+let que_count = 0;
+let counter;
+let timeValue = 25; // EXAM'S TIME
+let score = 0;
+Timer.textContent = timeValue;
 examT.textContent = `${timeValue} seconds`;
 
-nextQ.onclick = ()=>{
-    nextQ.style.display = "none" ;
-    timeStatus.textContent = "Time Left"
-    if(que_count < Questions.length - 1 ){
-        timeStatus.style.color = "var(--primary-color)";
-        timerBb.style.border = "2px solid var(--primary-color)"
-        Timer.style.background = "var(--primary-color)"
-        que_count ++ ;
-        showQuestions(que_count)
-        clearInterval(counter)
-        startTimer(timeValue) 
-    }else{
-        quizPage.classList.remove("active");
-        resultPage.classList.add("active");
-        result.textContent = `You Have Got ${score} Out of ${Questions.length}`
-    }
-    
-}
-
-
+nextQ.onclick = () => {
+  nextQ.style.display = "none";
+  timeStatus.textContent = "Time Left";
+  if (que_count < Questions.length - 1) {
+    timeStatus.style.color = "var(--primary-color)";
+    timerBb.style.border = "2px solid var(--primary-color)";
+    Timer.style.background = "var(--primary-color)";
+    que_count++;
+    showQuestions(que_count);
+    clearInterval(counter);
+    startTimer(timeValue);
+  } else {
+    quizPage.classList.remove("active");
+    resultPage.classList.add("active");
+    result.textContent = `You Have Got ${score} Out of ${Questions.length}`;
+  }
+};
 const showQuestions = (index) => {
-    // Question Number and Questions 
-    let questions = document.querySelector(".questions");
-    let queTag = `<span> ${Questions[index].numb}. ${Questions[index].question} </span>`;
-    questions.innerHTML = queTag ; 
-    // Options of Questions
-    let allOptions = document.querySelector(".allOptions");
-    let optTag = `
+  // Question Number and Questions
+  let questions = document.querySelector(".questions");
+  let queTag = `<span> ${Questions[index].numb}. ${Questions[index].question} </span>`;
+  questions.innerHTML = queTag;
+  // Options of Questions
+  let allOptions = document.querySelector(".allOptions");
+  let optTag = `
     <div class="option"><span>${Questions[index].options[0]}</span></div>
     <div class="option"><span>${Questions[index].options[1]}</span></div>
     <div class="option"><span>${Questions[index].options[2]}</span></div>
     <div class="option"><span>${Questions[index].options[3]}</span></div>
     `;
-    allOptions.innerHTML = optTag ; 
+  allOptions.innerHTML = optTag;
 
+  let totalQ = document.querySelector(".totalQ");
+  let totalTag = `${Questions[index].numb} of ${Questions.length}`;
+  totalQ.innerHTML = totalTag;
 
-    let totalQ = document.querySelector(".totalQ")
-    let totalTag = `${Questions[index].numb} of ${Questions.length}`;
-    totalQ.innerHTML = totalTag ;
-
-    let option = allOptions.querySelectorAll(".option")
-    for(let i=0; i<option.length; i++){
-        option[i].setAttribute("onclick","optionSelected(this)")
-    } 
-
-
-}
-
-let trueIcon = `<div> <i class="fas fa-check"></i> </div> `
-let falseIcon = ` <div> <i class="fas fa-times"></i> </div> `
-
+  let option = allOptions.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+};
+let trueIcon = `<div> <i class="fas fa-check"></i> </div> `;
+let falseIcon = ` <div> <i class="fas fa-times"></i> </div> `;
 const optionSelected = (ans) => {
-    nextQ.style.display = "block" ; 
-    clearInterval(counter)
-    let userAns = ans.textContent ; 
-    let correctAns = Questions[que_count].answer ; 
-    if(userAns===correctAns){
-        score += 1 ;
-        console.log(score)
-        ans.classList.add("true")
-        ans.insertAdjacentHTML("beforeend", trueIcon )
-    }else{
-        ans.classList.add("false")
-        ans.insertAdjacentHTML("beforeend", falseIcon )
-        let option = document.querySelectorAll(".option")
-        for(let i=0; i<option.length ; i++ ){
-            if(option[i].textContent === correctAns ){
-                option[i].setAttribute("class","option true")
-                option[i].insertAdjacentHTML("beforeend", trueIcon )
-            }
-        }
+  nextQ.style.display = "block";
+  clearInterval(counter);
+  let userAns = ans.textContent;
+  let correctAns = Questions[que_count].answer;
+  if (userAns === correctAns) {
+    score += 1;
+    console.log(score);
+    ans.classList.add("true");
+    ans.insertAdjacentHTML("beforeend", trueIcon);
+  } else {
+    ans.classList.add("false");
+    ans.insertAdjacentHTML("beforeend", falseIcon);
+    let option = document.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+      if (option[i].textContent === correctAns) {
+        option[i].setAttribute("class", "option true");
+        option[i].insertAdjacentHTML("beforeend", trueIcon);
+      }
+    }
+  }
+
+  let option = document.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].classList.add("disabled");
+  }
+};
+
+const startTimer = (time) => {
+  counter = setInterval(() => {
+    Timer.textContent = time;
+    time--;
+
+    if (time < 9) {
+      let i = Timer.textContent;
+      Timer.textContent = `0${i}`;
     }
 
-    let option = document.querySelectorAll(".option")
-    for(let i=0; i<option.length; i++){
-        option[i].classList.add("disabled")
+    if (time < 10) {
+      timeStatus.style.color = "var(--worning-color)";
+      timerBb.style.border = "2px solid var(--worning-color)";
+      Timer.style.background = "var(--worning-color)";
     }
-
-}
-
-const startTimer = (time)=>{
-    counter = setInterval(() => {
-        Timer.textContent = time ;
-        time-- ; 
-
-        if(time < 9){
-            let i = Timer.textContent ; 
-            Timer.textContent = `0${i}`
+    if (time < 0) {
+      clearInterval(counter);
+      Timer.textContent = "00";
+      timeStatus.textContent = "Time Off";
+      let ans = Questions[que_count].answer;
+      let option = document.querySelectorAll(".option");
+      for (let i = 0; i < option.length; i++) {
+        if (option[i].textContent === ans) {
+          option[i].setAttribute("class", "option true");
+          option[i].insertAdjacentHTML("beforeend", trueIcon);
         }
+      }
+      for (let i = 0; i < option.length; i++) {
+        option[i].classList.add("disabled");
+      }
+      nextQ.style.display = "block";
+    }
+  }, 1000);
+};
 
-        
-        
-
-        if(time < 10 ){
-            timeStatus.style.color = "var(--worning-color)";
-            timerBb.style.border = "2px solid var(--worning-color)"
-            Timer.style.background = "var(--worning-color)"
-        }
-        if(time <0 ){
-            clearInterval(counter)
-            Timer.textContent = "00" ;
-            timeStatus.textContent = "Time Off"; 
-            let ans = Questions[que_count].answer ; 
-            let option = document.querySelectorAll(".option") ;
-            for(let i=0; i<option.length; i++){
-                if(option[i].textContent === ans){
-                    option[i].setAttribute("class" , "option true" );
-                    option[i].insertAdjacentHTML("beforeend", trueIcon );
-                }
-                
-            }
-            for(let i=0; i<option.length; i++){
-                option[i].classList.add("disabled")
-            }
-            nextQ.style.display = "block";  
-        } 
-        
-    }, 1000);
-}
-
-const replayQuiz = ()=>{
-    que_count = 0 ;
-    score = 0 ; 
-    showQuestions(que_count)
-}
+const replayQuiz = () => {
+  que_count = 0;
+  score = 0;
+  showQuestions(que_count);
+};
